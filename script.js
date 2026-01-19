924a1cc310273debd4e5b50b87ebf199c82084df
rinatvisual <119620633+rinatvisual@users.noreply.github.com>
2026-01-19 08:06:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const reveals = document.querySelectorAll('.reveal');
 
 const observer = new IntersectionObserver(
-  (entries) => {
+  entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('active');
-        observer.unobserve(entry.target);
       }
     });
   },
-  {
-    threshold: 0.15
-  }
+  { threshold: 0.15 }
 );
 
-reveals.forEach(reveal => {
-  observer.observe(reveal);
-});
+reveals.forEach(el => observer.observe(el));
